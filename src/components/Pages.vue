00e97fedaa6112191dc0c3c2a7b689f9e6e52642
nellip <co.pfeiffer@gmail.com>
2020-08-28 16:43:34 +0200

    add pages component

---
+++ src/components/Pages.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row mt-3">
+    <div class="col">
+        categories
+    </div>
+    <div class="col-9">
+        {{pageContent}}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  components: {},
+  computed: {
+      ...mapState(["pages"]),
+      pageContent() {
+          let slug = this.$route.params.slug ? this.$route.params.slug : 'home';
+          let page = this.pages.find(page => page.slug == slug);
+
+          if (page){
+              return page.content;
+          }
+          return "";
+      }
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import Pages from '../components/Pages';
+
 Vue.use(VueRouter);
 
 export default new VueRouter({
-    mode: "history"
+    mode: "history",
+    routes: [
+        { path: "/:slug?", component: Pages },
+        { path: "*", redirect: "/" },
+    ]
 })
